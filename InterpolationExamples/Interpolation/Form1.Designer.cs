40ace4efe326fcecd72d41ee143eee8f49274ba3
Dan Miller <x.dan.miller@outlook.com>
2014-04-26 14:13:06 -0400

    Upscaling example.

--- InterpolationExamples/Interpolation/Form1.Designer.cs
+++ InterpolationExamples/Interpolation/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DrawImageButton = new System.Windows.Forms.Button();
+            this.BicubicButton = new System.Windows.Forms.Button();
+            this.Images = new System.Windows.Forms.PictureBox();
+            this.UpscaleButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Images)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawImageButton
+            // 
+            this.DrawImageButton.Location = new System.Drawing.Point(104, 388);
+            this.DrawImageButton.Name = "DrawImageButton";
+            this.DrawImageButton.Size = new System.Drawing.Size(75, 23);
+            this.DrawImageButton.TabIndex = 0;
+            this.DrawImageButton.Text = "Draw Image";
+            this.DrawImageButton.UseVisualStyleBackColor = true;
+            this.DrawImageButton.Click += new System.EventHandler(this.DrawImageButton_Click);
+            // 
+            // BicubicButton
+            // 
+            this.BicubicButton.Location = new System.Drawing.Point(361, 388);
+            this.BicubicButton.Name = "BicubicButton";
+            this.BicubicButton.Size = new System.Drawing.Size(75, 23);
+            this.BicubicButton.TabIndex = 1;
+            this.BicubicButton.Text = "Bi-cubic";
+            this.BicubicButton.UseVisualStyleBackColor = true;
+            this.BicubicButton.Click += new System.EventHandler(this.BicubicButton_Click);
+            // 
+            // Images
+            // 
+            this.Images.Location = new System.Drawing.Point(104, 35);
+            this.Images.Name = "Images";
+            this.Images.Size = new System.Drawing.Size(332, 312);
+            this.Images.TabIndex = 3;
+            this.Images.TabStop = false;
+            // 
+            // UpscaleButton
+            // 
+            this.UpscaleButton.Location = new System.Drawing.Point(226, 388);
+            this.UpscaleButton.Name = "UpscaleButton";
+            this.UpscaleButton.Size = new System.Drawing.Size(75, 23);
+            this.UpscaleButton.TabIndex = 4;
+            this.UpscaleButton.Text = "Upscale";
+            this.UpscaleButton.UseVisualStyleBackColor = true;
+            this.UpscaleButton.Click += new System.EventHandler(this.UpscaleButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 453);
+            this.Controls.Add(this.UpscaleButton);
+            this.Controls.Add(this.Images);
+            this.Controls.Add(this.BicubicButton);
+            this.Controls.Add(this.DrawImageButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Images)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button DrawImageButton;
+        private System.Windows.Forms.Button BicubicButton;
+        private System.Windows.Forms.PictureBox Images;
+        private System.Windows.Forms.Button UpscaleButton;
     }
 }
 
--- InterpolationExamples/Interpolation/Form1.cs
+++ InterpolationExamples/Interpolation/Form1.cs
@@ -16,5 +16,78 @@ namespace Interpolation
         {
             InitializeComponent();
         }
+
+        private void DrawImageButton_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(8, 8);
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                drawPrimary(g, 1, 0);
+                drawSecondary(g, 1, 1);
+                drawPrimary(g, 1, 2);
+                drawSecondary(g, 1, 3);
+                drawPrimary(g, 1, 4);
+                drawSecondary(g, 1, 5);
+                drawPrimary(g, 1, 6);
+                drawSecondary(g, 1, 7);
+            }
+
+            Images.Image = image;
+        }
+              
+        private void BicubicButton_Click(object sender, EventArgs e)
+        {
+            var original = Images.Image;
+            var scale = 10;
+            Bitmap newimg = new Bitmap(original.Width * scale, original.Height * scale);
+
+            using(Graphics g = Graphics.FromImage(newimg))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bicubic;
+                g.DrawImage(original, new Rectangle(Point.Empty, newimg.Size));
+            }
+
+            Images.Image = newimg;
+        }
+
+        private void UpscaleButton_Click(object sender, EventArgs e)
+        {
+            var original = Images.Image;
+            var scale = 10;
+            Bitmap newimg = new Bitmap(original.Width * scale, original.Height * scale);
+
+            using (Graphics g = Graphics.FromImage(newimg))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(original, new Rectangle(Point.Empty, newimg.Size));
+            }
+
+            Images.Image = newimg;
+        }
+
+        private void drawPrimary(Graphics g, int scale, int row)
+        {
+            g.FillRectangle(Brushes.DeepSkyBlue, 0 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.LawnGreen, scale, scale * row, scale, scale);
+            g.FillRectangle(Brushes.DeepSkyBlue, 2 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.LawnGreen, 3 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.DeepSkyBlue, 4 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.LawnGreen, 5 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.DeepSkyBlue, 6 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.LawnGreen, 7 * scale, row * scale, scale, scale);
+        }
+
+        private void drawSecondary(Graphics g, int scale, int row)
+        {
+            g.FillRectangle(Brushes.MediumVioletRed, 0 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.Gold, scale, scale * row, scale, scale);
+            g.FillRectangle(Brushes.MediumVioletRed, 2 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.Gold, 3 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.MediumVioletRed, 4 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.Gold, 5 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.MediumVioletRed, 6 * scale, row * scale, scale, scale);
+            g.FillRectangle(Brushes.Gold, 7 * scale, row * scale, scale, scale);
+        }
+
     }
 }
